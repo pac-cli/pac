9ad1adb7b6ee3046030ac9551ec01342a033562a
Cat <83475353+Vova-max-png@users.noreply.github.com>
2022-09-28 20:32:46 +0300

    Delete knowOptions, rename unparsedArgument to current_argument

--- src/options.cpp
+++ src/options.cpp
@@ -1,21 +1,21 @@
 #include "options.hpp"
-const std::regex knownOptions[] = {std::regex("-f"), std::regex("--file=.*")};
+
 void parse_options(std::vector<std::string> args){
     std::vector<unsigned int> ignore_list;
     printf("Size: %u\n", args.size());
-    for (unsigned int unparsedArgument_i = 0; unparsedArgument_i < args.size(); unparsedArgument_i++){
-        if (std::find(ignore_list.begin(),ignore_list.end(), unparsedArgument_i)!=ignore_list.end() || unparsedArgument_i==0) continue;
-        auto unparsedArgument = args[unparsedArgument_i];
+    for (unsigned int current_argument = 0; current_argument < args.size(); current_argument++){
+        if (std::find(ignore_list.begin(), ignore_list.end(), current_argument)!=ignore_list.end() || current_argument==0) continue;
+        auto unparsedArgument = args[current_argument];
         std::cmatch argumentMatch;
-        printf("Argument - %s: %u ",unparsedArgument.c_str(),std::regex_match(unparsedArgument.c_str(), argumentMatch, std::regex("-f")));
+        printf("Argument - %s: %u ", unparsedArgument.c_str(), std::regex_match(unparsedArgument.c_str(), argumentMatch, std::regex("-f")));
         if (std::regex_match(unparsedArgument.c_str(), argumentMatch, std::regex("-f"))) {
             printf("//Short file argument detected");
-            if (!(unparsedArgument_i+1 < args.size())) {
+            if (!(current_argument+1 < args.size())) {
                 printf("Error: Please provide filepath to read");
                 return;
             }
-            printf("\nPath: %s\n", args[unparsedArgument_i+1].c_str());
-            ignore_list.push_back(unparsedArgument_i+1);
+            printf("\nPath: %s\n", args[current_argument+1].c_str());
+            ignore_list.push_back(current_argument+1);
             continue;
         }
         if (std::regex_match(unparsedArgument.c_str(), argumentMatch, std::regex("--file=(.*)"))) {

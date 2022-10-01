25f8f07efe5044e2d1f241d69960e4c7de256548
Cat <83475353+Vova-max-png@users.noreply.github.com>
2022-10-01 17:08:26 +0300

    Fix options.cpp

--- src/options.cpp
+++ src/options.cpp
@@ -27,22 +27,12 @@ std::string parse_options(std::vector<std::string> args) {
 /*void parse_options(std::vector<std::string> args){
     std::vector<unsigned int> ignore_list;
     printf("Size: %u\n", args.size());
-<<<<<<< HEAD
-    for (unsigned int current_argument = 0; current_argument < args.size(); current_argument++){
-        printf(std::find(ignore_list.begin(), ignore_list.end(), current_argument));
-        if (std::find(ignore_list.begin(), ignore_list.end(), current_argument) != ignore_list.end() || current_argument==0) continue;
-        auto unparsedArgument = args[current_argument];
-        std::cmatch argumentMatch;
-        printf("Argument - %s: %u ", unparsedArgument.c_str(), std::regex_match(unparsedArgument.c_str(), argumentMatch, std::regex("-f")));
-        if (std::regex_match(unparsedArgument.c_str(), argumentMatch, std::regex("-f"))) {
-=======
     for (unsigned int current_argument_index = 0; current_argument_index < args.size(); current_argument_index++){
         if (std::find(ignore_list.begin(), ignore_list.end(), current_argument_index)!=ignore_list.end() || current_argument_index==0) continue;
         std::string current_argument = args[current_argument_index];
         std::cmatch current_argument_match_result;
         printf("Argument - %s: %u ", current_argument.c_str(), std::regex_match(current_argument.c_str(), current_argument_match_result, std::regex("-f")));
         if (std::regex_match(current_argument.c_str(), current_argument_match_result, std::regex("-f"))) {
->>>>>>> 639cf933e59203a62a7708a731574ddf51b52886
             printf("//Short file argument detected");
             if (!(current_argument_index+1 < args.size())) {
                 printf("Error: Please provide filepath to read");
